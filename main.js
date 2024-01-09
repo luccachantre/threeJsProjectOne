8d1dec8538982bb9398c746e07abf84b85013ad3
Lucca Chantre <luccachantre@gmail.com>
2024-01-09 14:27:30 -0500

    added lighting and a few more features, added a point light, ambient light, a helper polygon to show the light position, a grid, and orbit controls which allow a user to click and drag to move the camera, as well as zoom in and pan

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,13 +17,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30); //camera initializes in the middle of the scene(0,0,0)
 //so we want to back it up a bit so the camera can actually see items placed near and around the origin
 
+//remove this later maybe
+//camera.position.setY(15);
+
+
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true} );
+const material = new THREE.MeshStandardMaterial( {color: 0x3663D9} );
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 5, 0);
+pointLight.intensity = 200;
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight); 
+
+const lightHelper = new THREE.PointLightHelper(pointLight); //creates a basic wireframe polygon to show the location of the point light
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -30,6 +49,8 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
