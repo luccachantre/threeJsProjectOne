3cb0bbf5a2938268c53b775d5fb4c28210441633
Lucca Chantre <luccachantre@gmail.com>
2024-01-13 00:32:49 -0500

    did many things, reorganized the overall scene, moved objects, applied translations, rotations, adjusted lighting, etc, but the main feature is the the texture map added to the planet

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30); //camera initializes in the middle of the scene(0,0,0)
@@ -27,10 +28,10 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial( {color: 0x3663D9} );
 const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+//scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 5, 0);
+pointLight.position.set(6, 3, 6);
 pointLight.intensity = 200;
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 4);
@@ -63,6 +64,42 @@ scene.background = spaceTexture;
 //color was distorted (much lighter than original image) so I just set background to a solid color
 scene.background = new THREE.Color(0x230055);
 
+const dusknoirTexture = new THREE.TextureLoader().load('dusknoir.jpg');
+dusknoirTexture.colorSpace = THREE.SRGBColorSpace; //ok so this is needed for all textures or else the colors will be distorted upon loading
+//there might be a way to do this for the renderer instead of each individual texture, I'll have to look into this
+
+const dusknoir = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 5, 5),
+  new THREE.MeshBasicMaterial( {map: dusknoirTexture} )
+);
+
+
+scene.add(dusknoir);
+
+
+const earthTexture = new THREE.TextureLoader().load('earth.jpg');
+earthTexture.colorSpace = THREE.SRGBColorSpace;
+const normalTexture = new THREE.TextureLoader().load('earthNormal.jpg');
+
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial( {map: earthTexture, normalMap: normalTexture} )
+);
+
+scene.add(earth);
+//earth.translateY(10);
+//earth.translateX(23);
+//earth.translateZ(4);
+
+
+
+//some logic for transformations:
+//let's say you have an object that is not at the origin and you want to rotate it at its current position. 
+//you must first translate it to the origin, perform the rotation, and then translate it back to its original position
+
+dusknoir.translateX(-15);
+dusknoir.translateY(8);
+
 function animate() {
   requestAnimationFrame(animate);
 
@@ -70,8 +107,15 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  dusknoir.rotation.y += 0.01;
+
+  earth.rotation.y += 0.015;
 
   controls.update();
+
+  // camera.translateX(0.1);
+
+  //pointLight.translateX(0.1)
 
   renderer.render(scene, camera);
 }
